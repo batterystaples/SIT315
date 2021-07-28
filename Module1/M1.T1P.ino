f235fe5ad279017139f4baa0f90ce4c24ffc4d58
Riley Baird <rpbaird@deakin.edu.au>
2021-07-28 11:45:05 +1000

    Update M1.T1P code

--- Module1/M1.T1P.ino
+++ Module1/M1.T1P.ino
@@ -2,14 +2,22 @@ void setup()
 {
   pinMode(8, OUTPUT);
   pinMode(7, INPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
   int w = digitalRead(7);
   if (w == 0) {
-    digitalWrite(8, LOW);
+  	digitalWrite(8, LOW);
   } else {
     digitalWrite(8, HIGH);
+  }
+  if (millis() % 1000 == 0) {
+    if (w == 0) {
+      Serial.write("Button is currently not pressed, so light is off.\n");
+    } else {
+      Serial.write("Button is currently pressed, so light is on.\n");
+    }
   }
 }
